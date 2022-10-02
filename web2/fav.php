dd5034c9bb020e1fcaa4645c31b44ac854fa28f3
RalphDePaz <202080468@psu.palawan.edu.ph>
2022-10-02 23:57:36 +0800

    Done web2 and web3

--- web2/fav.php
+++ web2/fav.php
@@ -2,7 +2,18 @@
 <head><title>Your Favorite</title></head>
 <body>
     <?php
-        
+        if(!empty($_POST["username"])){ 
+            if(!empty($_POST["dish"])){
+                if (!empty($_POST["color"])){
+                echo ("Thanks for your selection " . $_POST["username"] . "</br>");
+                echo ("You really enjoyed " . $_POST["dish"] . "</br>");
+                echo ("- especially with a nice " . $_POST["color"] . " wine" . "</br>");
+            }else {echo "Please fill up all, thank you.";}
+            }else {echo "Please fill up all, thank you.";}
+        }
+        else {
+            echo "Please fill up all, thank you.";
+        }
     ?>
 </body>
 </html>
--- web3/calc.php
+++ web3/calc.php
@@ -3,12 +3,27 @@
 <body>
     <?php
         # test if input is_numeric
-        if(1){
+        if(is_numeric($_POST["val1"]) && is_numeric($_POST["val2"]) && !empty($_POST["val1"]) && !empty($_POST["val2"])){
             # test if calc is not null
-            if(1){
+            if(isset($_POST["calc"])){
+                $num1 = $_POST["val1"];
+                $num2 = $_POST["val2"];
+                $result = $_POST["calc"];
                 # switch statement for value of calc
-                switch(1){
-                    
+                switch($result){
+                    case "add":
+                        $result = $num1 + $num2;
+                        break;
+                    case "sub":
+                        $result = $num1 - $num2;
+                        break;
+                    case "mul":
+                        $result = $num1 * $num2;
+                        break;
+                    case "div":
+                        $result = $num1 / $num2;
+                        break;
+
                 }
                 echo "Calculation result: $result";
             }
