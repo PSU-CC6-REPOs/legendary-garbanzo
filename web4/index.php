a3f82908bc1d488a11c440de7cd04e3d8d44561d
John Dominic Nuñez <90952117+Domzzzzz@users.noreply.github.com>
2023-03-03 00:07:15 +0800

    Update index.php

--- web4/index.php
+++ web4/index.php
@@ -1,6 +1,22 @@
 <?php
-// set cookies
+if(isset($_POST['submit'])) {
+  // get the input values
+  $username = $_POST['username'];
+  $password = $_POST['password'];
 
+  // check if both inputs are not empty
+  if(!empty($username) && !empty($password)) {
+    // set the auth cookie with a value of "ok"
+    setcookie('auth', 'ok');
+
+    // redirect to the logged in page
+    header('Location: loggedin.php');
+    exit();
+  } else {
+    // prompt to fill in both fields if either is empty
+    $message = "Please enter both your username and password.";
+  }
+}
 ?>
 <html lang="en">
 <head>
@@ -9,10 +25,19 @@
     <title>Set Cookie Data</title>
 </head>
 <body>
-    <form action="<?php //<code>// ?>" method="">
-    Name: <input type="text" name="username">
-    Password: <input type="text" name="password"><br/><br/>
-    <input type="submit" value="Log Me In">
-    </form>
+   <form method="post">
+  <label for="username">Username:</label>
+  <input type="text" id="username" name="username" required>
+  <br>
+  <label for="password">Password:</label>
+  <input type="password" id="password" name="password" required>
+  <br>
+  <button type="submit" name="submit">Log In</button>
+</form>
+<?php if(isset($message)) { echo $message; } ?>
 </body>
 </html>
+
+
+
+
