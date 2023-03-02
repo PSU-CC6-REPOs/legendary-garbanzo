d468e9509996314b4f41d5c64948e4f6f2c83598
John Dominic Nuñez <90952117+Domzzzzz@users.noreply.github.com>
2023-03-03 00:18:08 +0800

    Update index.php

--- web5/index.php
+++ web5/index.php
@@ -1,6 +1,19 @@
 <?php
-
+session_start(); 
+if(!isset($_SESSION['visits'])) {
+  $_SESSION['visits'] = 0; 
+}
+$_SESSION['visits']++; 
+if($_SESSION['visits'] == 5) {
+  echo "Thank you for visiting the page a lot.";
+}
+if($_SESSION['visits'] == 10) {
+  header('Location: congratulations.php'); 
+  exit();
+}
+echo "Number of visits: " . $_SESSION['visits']; 
 ?>
+
 
 <h1>Hello</h1>
 <p>You have visited this page <?= $num_visits ?> times</p>
